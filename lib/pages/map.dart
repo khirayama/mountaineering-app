844d5bdcf3d65ea66d00c37bdb572d76414cae8c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-05-16 17:34:34 +0900

    Display map

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
 import '../components/navigation.dart';
 
@@ -14,11 +15,12 @@ class _MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Map'),
-          ],
+        child: MapboxMap(
+          accessToken: 'pk.eyJ1Ijoia2hpcmF5YW1hIiwiYSI6ImNqa2YycGZ1ejA0bnQzdm8za2w3dnkxdmwifQ.Xf4dKzaKSxv_CTDqkyt_ug',
+          initialCameraPosition: CameraPosition(
+            zoom: 15.0,
+            target: LatLng(14.508, 46.048),
+          ),
         ),
       ),
       bottomNavigationBar: Navigation(index: 1),
