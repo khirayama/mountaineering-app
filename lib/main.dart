69851dd6341df9c3806629e6199be3f12f358dad
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-05-16 09:21:50 +0900

    Use MaterialAPp to bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 import './home_page.dart';
+import './map_page.dart';
+import './profile_page.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: <String, WidgetBuilder> {
+        '/': (BuildContext context) => HomePage(),
+        '/map': (BuildContext context) => MapPage(),
+        '/profile': (BuildContext context) => ProfilePage(),
+      },
       title: 'Flutter Demo',
       theme: new ThemeData(
         primaryColor: Colors.white,
@@ -20,7 +23,6 @@ class MyApp extends StatelessWidget {
           },
         ),
       ),
-      home: HomePage(),
-    );
-  }
+    )
+  );
 }
