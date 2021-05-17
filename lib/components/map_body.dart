9c73787f4f892b446fc057450c1893023c52d4b5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-05-18 07:08:03 +0900

    Display map correctly

--- lib/components/map_body.dart
+++ lib/components/map_body.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 import '../secret.dart';
+// import '../map_style.dart';
+
+CameraPosition _cachedCameraPosition = CameraPosition(
+  zoom: 9.0,
+  target: LatLng(14.508, 46.048),
+);
 
 class MapBody extends StatefulWidget {
-  MapBody({Key key, this.index}) : super(key: key);
-
-  final int index;
+  MapBody({Key key}) : super(key: key);
 
   @override
   _MapBodyState createState() => _MapBodyState();
@@ -15,48 +19,38 @@ class MapBody extends StatefulWidget {
 class _MapBodyState extends State<MapBody> {
   MapboxMapController _controller;
 
-  double _zoom = 15.0;
+  void _onMapChanged() {
+    _cachedCameraPosition = _controller.cameraPosition;
+    setState(() {
+      _cameraPosition = _controller.cameraPosition;
+    });
+  }
 
-  LatLng _target = LatLng(14.508, 46.048);
+  CameraPosition _cameraPosition = _cachedCameraPosition;
 
   @override
   Widget build(BuildContext context) {
+    final MapboxMap mapboxMap = MapboxMap(
+      accessToken: MAPBOX_ACCESS_TOKEN,
+      initialCameraPosition: _cameraPosition,
+      trackCameraPosition: true,
+      onMapCreated: (MapboxMapController controller) {
+        _controller = controller;
+        _cameraPosition = controller.cameraPosition;
+        _controller.addListener(_onMapChanged);
+      },
+    );
+
     return Stack(
       children: [
-        Center(
-          child: MapboxMap(
-            accessToken: MAPBOX_ACCESS_TOKEN,
-            initialCameraPosition: CameraPosition(
-              zoom: _zoom,
-              target: _target,
-            ),
-            onMapCreated: (MapboxMapController controller) {
-              _controller = controller;
-            },
-            // onMapLongClick: (context) {
-            //   print('maplongclick');
-            // },
-            onUserLocationUpdated: (context) {
-              print('userlocationupdated');
-            },
-            onCameraIdle: () {
-              print('--- cameraidle ---');
-              print(_controller.cameraPosition);
-              setState(() {
-                _zoom = _controller.cameraPosition.zoom;
-                _target = _controller.cameraPosition.target;
-              });
-            },
-          ),
-        ),
+        Center(child: mapboxMap),
         Column(
           children: [
-            Text(_zoom.toString()),
-            Text(_target.toString()),
+            Text(_cameraPosition != null ? _cameraPosition.zoom.toString() : 'null'),
+            Text(_cameraPosition != null ? _cameraPosition.target.toString() : 'null'),
           ],
         ),
       ],
     );
-
   }
 }
